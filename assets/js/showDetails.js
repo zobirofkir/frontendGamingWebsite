9b80569b58c6707375a169e36c7ffcbb9ab9592f
zobirofkir <zobirofkir19@gmail.com>
2023-12-04 20:07:21 +0100

    update show details ...

--- assets/js/showDetails.js
+++ assets/js/showDetails.js
@@ -1,7 +1,7 @@
 function showGameDetails(gameId){
     $.ajax({
         method:"GET",
-        url:`http://127.0.0.1:8000/api/data/${gameId}`,
+        url:`https://zobirofkir.com/api/api/data/${gameId}`,
         dataType: "json",
         success: function(gameData){
             showGameDetails.html("");
